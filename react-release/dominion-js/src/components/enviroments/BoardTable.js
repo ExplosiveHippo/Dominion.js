592d118397bec075332d1786bdc12e759a83d53d
Lance Laughlin <lancelaughlin@gmail.com>
2017-01-31 10:59:48 -0500

    JSON Data structure for cards

--- react-release/dominion-js/src/components/enviroments/BoardTable.js
+++ react-release/dominion-js/src/components/enviroments/BoardTable.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import OrganismCard from '../organisms/Organism.card';
 
 export default class BoardTable extends Component {
-  render() {
-    return (
-    	<OrganismCard />
-    );
-  }
+	constructor(props){
+		super(props);
+		this.state = {
+
+		};
+	}
+
+	render() {
+		return (
+			<OrganismCard />
+		);
+	}
 }
--- react-release/dominion-js/src/components/organisms/Organism.card.js
+++ react-release/dominion-js/src/components/organisms/Organism.card.js
@@ -5,7 +5,6 @@ import imageMap from '../../imageMap';
 export default class ExampleOrganism extends Component {
   render() {
     return (
-
       <img src={imageMap.bazaar} />
     );
   }

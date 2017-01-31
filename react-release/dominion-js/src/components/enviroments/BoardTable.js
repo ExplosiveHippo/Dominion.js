1656450e472195f4148644cec28310bd6eb14fb2
Lance Laughlin <lancelaughlin@gmail.com>
2017-01-31 11:18:01 -0500

    Should probably have valid JSON data

--- react-release/dominion-js/src/components/enviroments/BoardTable.js
+++ react-release/dominion-js/src/components/enviroments/BoardTable.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import OrganismCard from '../organisms/Organism.card';
+import cardJSON from '../../cardData';
 
 export default class BoardTable extends Component {
-	constructor(props){
-		super(props);
+
+	constructor() {
+		super();
 		this.state = {
 
 		};
+		console.log(cardJSON);
 	}
 
 	render() {
